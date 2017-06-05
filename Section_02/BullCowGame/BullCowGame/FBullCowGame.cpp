3f8808ffb301b4d0e377678931cc4d59a4d93841
Steven Rogers <stevenr180@gmail.com>
2017-06-05 12:41:06 -0700

    Moving forward in the lectures. Added logic for the game guessing the word.

--- Section_02/BullCowGame/BullCowGame/FBullCowGame.cpp
+++ Section_02/BullCowGame/BullCowGame/FBullCowGame.cpp
@@ -8,12 +8,14 @@
 
 #include "FBullCowGame.hpp"
 
+using int32 = int;
+
 // Constructor, runs when new instance is created
 FBullCowGame::FBullCowGame() { Reset(); }
 
 // Getter functions
-int FBullCowGame::GetMaxTries() const { return MyMaxTries; }
-int FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
+int32 FBullCowGame::GetMaxTries() const { return MyMaxTries; }
+int32 FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
 
 // Resets the variables of the class back to default
 void FBullCowGame::Reset()
@@ -21,16 +23,58 @@ void FBullCowGame::Reset()
     
     MyCurrentTry = 1;
     MyMaxTries = 5;
+    MyHiddenWord = "plate";
     return;
 }
 
 
+// Checks if the game has been won
 bool FBullCowGame::IsGameWon() const
 {
     return true;
 }
 
-bool FBullCowGame::CheckGuessValidity(std::string) {
+// Makes sure that the guess is valid
+bool FBullCowGame::CheckGuessValidity(FString) {
     return true;
 }
 
+// Assumes a valid guess
+FBullCowCount FBullCowGame::SubmitGuess(FString Guess) {
+    
+    // Increment the Turn number
+    MyCurrentTry++;
+    
+    // Setup a return structure (struct)
+    FBullCowCount BCCount;
+    BCCount.Bull = 0;
+    BCCount.Cow = 0;
+    
+    // Get the length of the hidden word;
+    int32 HiddenWordLength = MyHiddenWord.length();
+    
+    // Loop through all the letters in the hidden word
+    for (int32 i = 0; i < HiddenWordLength; i++) {
+        
+        // Loop through all the letters in the guess
+        for (int32 j = 0; j < HiddenWordLength; j++) {
+            
+            // If the letter matches...
+            if (MyHiddenWord[i] == Guess[j]) {
+                
+                // Find out if the letter is in the correct spot
+                if (j == i) {
+                    BCCount.Bull++;
+                } else {
+                    BCCount.Cow++;
+                }
+                
+                break;
+            }
+        }
+    }
+    
+    
+    
+    return BCCount;
+}
